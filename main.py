fc3c752b894c02b6e96821b1a47dfad2600ed566
seeker8 <chuyo_gk87@yahoo.com.mx>
2026-01-30 00:45:25 -0500

    follow along code

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.title("Snake Game")
 screen.bgcolor("black")
-jiji = Turtle(shape="square")
-jiji.color("white")
-jiji.penup()
+screen.setup(width=600, height=600)
+screen.tracer(0)
 
-stamp_id = 0
-def draw_snake(length):
-    for i in range(length):
-        jiji.stamp()
-        jiji.forward(20)
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+segments = []
 
-for _ in range(10):
-    jiji.clearstamps()
-    draw_snake(3)
+for position in starting_positions:
+    segment = Turtle("square")
+    segment.penup()
+    segment.color("white")
+    segment.goto(position)
+    segments.append(segment)
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    for segment_num in range(len(segments)-1, 0, -1):
+        segments[segment_num].goto(segments[segment_num-1].xcor(), segments[segment_num-1].ycor())
+    segments[0].forward(20)
+    segments[0].left(90)
 
 
 
